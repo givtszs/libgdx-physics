49426acd1e84c0c7ce32691d0d87dc5e4c4995cf
givtszs <givtszs@gmail.com>
2023-11-18 22:10:51 +0200

    Add camera and viewport to support scaling.
    Corrected useAndroidX property misspelling in the gradle.properties file.

--- core/src/com/mygdx/game/LGPhysics.java
+++ core/src/com/mygdx/game/LGPhysics.java
@@ -1,19 +1,26 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class LGPhysics extends ApplicationAdapter {
 
 	Sprite banana;
 	SpriteBatch batch;
 	TextureAtlas textureAtlas;
+	OrthographicCamera camera;
+	ExtendViewport viewport;
 	
 	@Override
 	public void create () {
+		camera = new OrthographicCamera();
+		viewport = new ExtendViewport(800, 600, camera);
 		batch = new SpriteBatch();
 		textureAtlas = new TextureAtlas("sprites.txt");
 		banana = textureAtlas.createSprite("banana");
@@ -31,5 +38,11 @@ public class LGPhysics extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		textureAtlas.dispose();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		viewport.update(width, height, true);
+		batch.setProjectionMatrix(camera.combined);
 	}
 }
