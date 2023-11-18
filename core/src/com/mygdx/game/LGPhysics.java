83238299e3434e2ebb3ddea43c9798ab25d4e18e
givtszs <givtszs@gmail.com>
2023-11-18 22:33:35 +0200

    Add helper methods to load and draw sprites.

--- core/src/com/mygdx/game/LGPhysics.java
+++ core/src/com/mygdx/game/LGPhysics.java
@@ -5,17 +5,19 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
-public class LGPhysics extends ApplicationAdapter {
+import java.util.HashMap;
 
-	Sprite banana;
+public class LGPhysics extends ApplicationAdapter {
 	SpriteBatch batch;
 	TextureAtlas textureAtlas;
 	OrthographicCamera camera;
 	ExtendViewport viewport;
+	final HashMap<String, Sprite> sprites = new HashMap<>();
 	
 	@Override
 	public void create () {
@@ -23,21 +25,35 @@ public class LGPhysics extends ApplicationAdapter {
 		viewport = new ExtendViewport(800, 600, camera);
 		batch = new SpriteBatch();
 		textureAtlas = new TextureAtlas("sprites.txt");
-		banana = textureAtlas.createSprite("banana");
+		loadSprites();
+	}
+
+	private void loadSprites() {
+		for (TextureAtlas.AtlasRegion region: textureAtlas.getRegions()) {
+			sprites.put(region.name, textureAtlas.createSprite(region.name));
+		}
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(0.57f, 0.77f, 0.85f, 1);
 		batch.begin();
-		banana.draw(batch);
+		drawSprite(sprites.get("banana"), 0, 0);
+		drawSprite(sprites.get("banana"), 130, 0);
+		drawSprite(sprites.get("cherries"), 0, 130);
 		batch.end();
 	}
-	
+
+	private void drawSprite(Sprite sprite, float x, float y) {
+		sprite.setPosition(x, y);
+		sprite.draw(batch);
+	}
+
 	@Override
 	public void dispose () {
 		batch.dispose();
 		textureAtlas.dispose();
+		sprites.clear();
 	}
 
 	@Override
