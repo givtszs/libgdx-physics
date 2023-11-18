cefd6b5a86b2205432b7415a4c2d9b9641204ef7
givtszs <givtszs@gmail.com>
2023-11-18 21:43:12 +0200

    Added sprite sheet. Creating and drawing a sprite in the core class.

--- core/src/com/mygdx/game/LGPhysics.java
+++ core/src/com/mygdx/game/LGPhysics.java
@@ -1,31 +1,35 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class LGPhysics extends ApplicationAdapter {
+
+	Sprite banana;
 	SpriteBatch batch;
-	Texture img;
+	TextureAtlas textureAtlas;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		textureAtlas = new TextureAtlas("sprites.txt");
+		banana = textureAtlas.createSprite("banana");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0.57f, 0.77f, 0.85f, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		banana.draw(batch);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		textureAtlas.dispose();
 	}
 }
